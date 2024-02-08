99beafed869e2afe88dea460f0f707488a950f51
Karimatif33 <karimatif33@gmail.com>
2024-02-08 07:45:04 +0200

    fetching api data

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,5 +1,20 @@
-function Menu() {
-  return <h1>Menu</h1>;
-}
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant.js";
+import MenuItem from "./MenuItem.jsx";
 
+function Menu() {
+  const menu = useLoaderData();
+  console.log(menu);
+  return (
+    <ul>
+      {menu.map((pizza) => (
+        <MenuItem pizza={pizza} key={pizza.id} />
+      ))}
+    </ul>
+  );
+}
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
+}
 export default Menu;
