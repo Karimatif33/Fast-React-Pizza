e327514ad43dd58e40aef3140de73edf7af2f776
Karimatif33 <karimatif33@gmail.com>
2024-02-10 06:35:40 +0200

    fetching api data

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./ui/Home";
+import Error from "./ui/Error";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
-import Order from "./features/order/Order";
+import Order , {loader as orderLoader} from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       {
         path: "/",
@@ -18,6 +20,7 @@ const router = createBrowserRouter([
         path: "/menu",
         element: <Menu />,
         loader: menuLoader,
+        errorElement: <Error />
       },
       {
         path: "/cart",
@@ -28,8 +31,9 @@ const router = createBrowserRouter([
         element: <CreateOrder />,
       },
       {
-        path: "/order/:id",
-        element: <Order />,
+        path: "/order/:orderId",
+        element: <Order />, loader: orderLoader,
+        errorElement: <Error />
       },
     ],
   },
