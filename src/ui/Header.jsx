82144b1eba6f4f18bdf05c26d02d5848d72b45a4
Karimatif33 <karimatif33@gmail.com>
2024-02-11 05:49:11 +0200

    formating with tailwind

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 function Header() {
   return (
-    <header>
-      <Link to="/">Fast React Pizza Co.</Link>
-      <SearchOrder/>
-      <p>Pizza</p>
+    <header className="bg-yellow-600 uppercase ">
+      <Link to="/" className="tracking-widest">
+        Fast React Pizza Co.
+      </Link>
+      <SearchOrder />
+    <Username />
     </header>
   );
 }
