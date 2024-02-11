6d2fc8147f9cf2704c15a2f58c68d76d936f1780
Karimatif33 <karimatif33@gmail.com>
2024-02-11 06:15:46 +0200

    workink with flex and grid

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,8 +1,8 @@
 import { Link } from "react-router-dom";
 function CartOverview() {
   return (
-    <div className="bg-stone-800 text-stone-200 uppercase">
-      <p className="font-semibold	 text-stone-300">
+    <div className="bg-stone-800 p-4 uppercase text-stone-200 sm:px-6 px-4 py-4 text-sm md-text-base flex items-center justify-between">
+      <p className="space-x-4	 font-semibold text-stone-300 sm:space-x-6">
         <span>23 pizzas</span>
         <span>$23.45</span>
       </p>
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,12 +4,12 @@ import Username from "../features/user/Username";
 
 function Header() {
   return (
-    <header className="bg-yellow-600 uppercase ">
+    <header className="border-b border-stone-200 bg-yellow-600 px-4 py-3 uppercase sm:px-6">
       <Link to="/" className="tracking-widest">
         Fast React Pizza Co.
       </Link>
       <SearchOrder />
-    <Username />
+      <Username />
     </header>
   );
 }
